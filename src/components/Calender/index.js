ff918d5b3fdd4c7d04d2abbad05c8eddc1e51fc3
suruchi-sihra <suruchikaur.sihra@netmeds.net>
2021-05-27 20:42:55 +0530

    added date

--- src/components/Calender/index.js
+++ src/components/Calender/index.js
@@ -1,75 +1,105 @@
-
 import React from 'react';
-import Scheduler, { AppointmentDragging } from 'devextreme-react/scheduler';
-import ListViewAppointments from '../ListViewAppointments';
-import { data } from '../../shared/data';
-import ScrollView from 'devextreme-react/scroll-view';
-import Draggable from 'devextreme-react/draggable';
+import Scheduler from 'devextreme-react/scheduler';
+import { generateAppointments, holidays } from '../../shared/data';
+import Utils from './util';
+import DataCell from './DataCell.js';
+import DateCell from './DateCell.js';
+import TimeCell from './TimeCell.js';
+import notify from 'devextreme/ui/notify';
 
 const currentDate = new Date(2021, 4, 27);
-const views = ['day', 'week', 'workWeek', 'month'];
-const draggingGroupName = 'appointmentsGroup';
+const views = [
+  { type: 'day', name: 'Day', intervalCount: 1 },
+  { name: '4 Days', type: 'day', intervalCount: 4 },
+  { name: 'Month', type: 'month', intervalCount: 1 },
+  { name: 'Day List View', type: 'agenda', agendaDuration: 1, }
+];
 
-class Calender extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: data
-        };
+const scrolling = { mode: 'virtual' };
+const startDay = new Date(2021, 4, 1);
+const endDay = new Date(2021, 4, 28);
+const startDayHour = 6;
+const endDayHour = 24;
+
+const appointments = generateAppointments(startDay, endDay, startDayHour, endDayHour);
+
+function App() {
+
+  const notifyDisableDate = () => {
+    notify('Cannot create or move an appointment/event to disabled time/date regions.', 'warning', 1000);
+  }
+
+
+  const onAppointmentFormOpening = (e) => {
+    const startDate = new Date(e.appointmentData.startDate);
+    if (!Utils.isValidAppointmentDate(startDate)) {
+      e.cancel = true;
+      notifyDisableDate();
     }
+    applyDisableDatesToDateEditors(e.form);
+  }
 
-    onListDragStart(e) {
-        e.cancel = true;
+  const onAppointmentAdding = (e) => {
+    const isValidAppointment = Utils.isValidAppointment(e.component, e.appointmentData);
+    if (!isValidAppointment) {
+      e.cancel = true;
+      notifyDisableDate();
     }
+  }
 
-    onItemDragStart(e) {
-        e.itemData = e.fromData;
+  const onAppointmentUpdating = (e) => {
+    const isValidAppointment = Utils.isValidAppointment(e.component, e.newData);
+    if (!isValidAppointment) {
+      e.cancel = true;
+      notifyDisableDate();
     }
+  }
 
-    onItemDragEnd(e) {
-        if (e.toData) {
-            e.cancel = true;
-        }
-    }
+  const applyDisableDatesToDateEditors = (form) => {
+    const startDateEditor = form.getEditor('startDate');
+    startDateEditor.option('disabledDates', holidays);
 
-    render() {
+    const endDateEditor = form.getEditor('endDate');
+    endDateEditor.option('disabledDates', holidays);
+  }
 
-        return (
-            <div>
-                { data.length <= 50 ?
-                    <React.Fragment>
-                        <ScrollView id="scroll">
-                            <Draggable
-                                id="list"
-                                data="dropArea"
-                                group={draggingGroupName}
-                                onDragStart={this.onListDragStart}>
-                            </Draggable>
-                        </ScrollView>
-                        <Scheduler
-                            timeZone="Asia/Kolkata"
-                            dataSource={data}
-                            views={views}
-                            defaultCurrentView="day"
-                            defaultCurrentDate={currentDate}
-                            height={600}
-                            startDayHour={9}
-                            editing={true}>
-                            <AppointmentDragging
-                                group={draggingGroupName}
-                                onRemove={this.onAppointmentRemove}
-                                onAdd={this.onAppointmentAdd}
-                            />
-                        </Scheduler>
-                    </React.Fragment>
-                    :
-                    <ListViewAppointments />
-                }
-            </div>
 
-        );
-    }
+  const renderDataCell = (itemData) => {
+    return <DataCell itemData={itemData} />;
+  }
+
+  const renderDateCell = (itemData) => {
+    return <DateCell itemData={itemData} />;
+  }
+
+  const renderTimeCell = (itemData) => {
+    return <TimeCell itemData={itemData} />;
+  }
+
+
+  return (
+    <Scheduler
+      timeZone="Asia/Kolkata"
+      dataSource={appointments}
+      height={600}
+      views={views}
+      defaultCurrentView="day"
+      showCurrentTimeIndicator={true}
+      shadeUntilCurrentTime={true}
+      defaultCurrentDate={currentDate}
+      startDayHour={startDayHour}
+      endDayHour={endDayHour}
+      cellDuration={50}
+      showAllDayPanel={true}
+      dataCellRender={renderDataCell}
+      dateCellRender={renderDateCell}
+      timeCellRender={renderTimeCell}
+      onAppointmentFormOpening={onAppointmentFormOpening}
+      onAppointmentAdding={onAppointmentAdding}
+      onAppointmentUpdating={onAppointmentUpdating}
+    />
+  );
 }
 
-export default Calender;
+export default App;
